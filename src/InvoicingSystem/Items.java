1b091d67ebadca52a54dc89a9f367716c70ce62b
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-01-22 23:10:13 -0800

    add new

--- src/InvoicingSystem/Items.java
+++ src/InvoicingSystem/Items.java
@@ -1,5 +1,51 @@
 package InvoicingSystem;
 
 public class Items {
+	
+	String itemName;
+	int itemID;
+	int quantity;
+	double unitPrice;
+	double qtyAmountPrice;
+	
+	
+	public String getItemName() {
+	    return itemName;
+	  }
+	  public void setItemName(String newItemName) {
+	    this.itemName = newItemName;
+	  }
+	  
+	  
+	  public int getItemID() {
+		    return itemID;
+		  }
+		  public void setItemID(int newItemID) {
+		    this.itemID = newItemID;
+		  }
+		  
+		  
+		  public int getQuantity() {
+			    return quantity;
+			  }
+			  public void setQuantity(int newQuantity) {
+			    this.quantity = newQuantity;
+			  }
+			  
+			  
+			  public double getUnitPrice() {
+				    return unitPrice;
+				  }
+				  public void setUnitPrice(double newUnitPrice) {
+				    this.unitPrice = newUnitPrice;
+				  }
+				  
+				  
+				  public double getQtyAmountPrice() {
+					    return qtyAmountPrice;
+					  }
+					  public void setQtyAmountPrice(double newQtyAmountPrice) {
+					    this.qtyAmountPrice = newQtyAmountPrice;
+					  }
 
 }
